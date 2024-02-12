31c9b4874b65bf7a9e18458c387802fa854465eb
Tom5521 <thomas.bored5521@gmail.com>
2024-02-12 12:55:25 -0300

    Add GtkButton example

--- GtkButton/main.go
+++ GtkButton/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/diamondburned/gotk4/pkg/gio/v2"
-	"github.com/diamondburned/gotk4/pkg/gtk/v3"
+	"github.com/diamondburned/gotk4/pkg/gtk/v4"
 )
 
 func main() {
@@ -18,5 +19,28 @@ func main() {
 }
 
 func activate(app *gtk.Application) {
+	w := gtk.NewApplicationWindow(app)
 
+	box := gtk.NewBox(gtk.OrientationVertical, 6)
+
+	// two ways to declare a button
+	button1, button2 := gtk.NewButton(), gtk.NewButtonWithLabel("Hello World")
+
+	// You can set the button label with SetLabel method
+	button1.SetLabel("Button 1")
+
+	// You can connect the button clicks with the ConnectClick method.
+	button1.ConnectClicked(func() {
+		fmt.Println(button1.Label())
+	})
+
+	button2.ConnectClicked(func() {
+		fmt.Println(button2.Label())
+	})
+
+	box.Append(button1)
+	box.Append(button2)
+
+	w.SetChild(box)
+	w.Show()
 }
