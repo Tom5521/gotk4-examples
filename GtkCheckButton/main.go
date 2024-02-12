7d0506ce99e1aaff3c61640e44c5ef8c19f481f3
Tom5521 <thomas.bored5521@gmail.com>
2024-02-12 20:13:09 -0300

    Add GtkCheckButton

--- GtkCheckButton/main.go
+++ GtkCheckButton/main.go
@@ -23,35 +23,20 @@ func activate(app *gtk.Application) {
 
 	box := gtk.NewBox(gtk.OrientationVertical, 6)
 
-	tv := gtk.NewTextView()
-	tv.SetVExpand(true)
-	tb := tv.Buffer()
+	// two ways to declare a CheckButton
+	check1, check2 := gtk.NewCheckButton(), gtk.NewCheckButtonWithLabel("Hi world")
 
-	tb.SetText("Hello World!")
-	tv.SetWrapMode(gtk.WrapWordChar)
-
-	button := gtk.NewButtonWithLabel("Save data")
-	button.ConnectClicked(func() {
-		err := os.WriteFile(
-			"buffer.log",
-			[]byte(
-				tb.Text(
-					tb.StartIter(),
-					tb.EndIter(),
-					false,
-				),
-			),
-			os.ModePerm,
-		)
-		if err != nil {
-			fmt.Println(err)
-		}
+	check1.ConnectToggled(func() {
+		mode := check1.Active()
+		fmt.Println("check1:", mode)
+	})
+	check2.ConnectToggled(func() {
+		mode := check2.Active()
+		fmt.Println("check2:", mode)
 	})
 
-	sw := gtk.NewScrolledWindow()
-	sw.SetChild(tv)
-	box.Append(sw)
-	box.Append(button)
+	box.Append(check1)
+	box.Append(check2)
 
 	w.SetChild(box)
 	w.Show()
