38d5b250c287b646dff132779dd4e881ffd8d66d
Tom5521 <thomas.bored5521@gmail.com>
2024-02-22 05:26:27 -0300

    Add GtkVideo example

---
+++ GtkVideo/README.md
@@ -0,0 +1,8 @@
+## Link to the documentation
+
+<https://docs.gtk.org/gtk4/class.Video.html>
+
+Note: The usage examples and functions, etc. are in C.
+
+
+Video source: <https://youtu.be/IOEfrt0EuWE?si=dCDusrjqKuf5_h8P>
---
+++ GtkVideo/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+
+	"github.com/diamondburned/gotk4/pkg/gio/v2"
+	"github.com/diamondburned/gotk4/pkg/gtk/v4"
+)
+
+func main() {
+	app := gtk.NewApplication("com.test.window", gio.ApplicationFlagsNone)
+	app.ConnectActivate(func() {
+		activate(app)
+	})
+	if code := app.Run(os.Args); code > 0 {
+		os.Exit(code)
+	}
+}
+
+func activate(app *gtk.Application) {
+	w := gtk.NewApplicationWindow(app)
+	w.SetDefaultSize(500, 500)
+	w.SetTitle("Example video")
+
+	video := gtk.NewVideoForFilename("GtkVideo/video.mp4")
+
+	w.SetChild(video)
+	w.Show()
+}
