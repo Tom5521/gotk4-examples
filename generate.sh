4fcc0f64adb0ba0be0e228045973a171e2d74cf3
Tom5521 <thomas.bored5521@gmail.com>
2024-03-02 14:12:33 -0300

    Add help message to generate.sh

--- generate.sh
+++ generate.sh
@@ -5,6 +5,15 @@ if [ "$1" == "" ]; then
 	exit 1
 fi
 
+if [ "$1" == "help" ]; then
+	echo This creates the folder, the README.md, and a main.go template for an example, \
+		I use it to generate new examples quickly, because before what I did was pure copy-paste.
+	echo
+	echo The use is simple, just run ./generate.sh [class name] \
+		and the script creates the folder and the corresponding readme.
+	exit 0
+fi
+
 if [ -d "$1" ]; then
 	echo The folder already exists
 	exit 1
